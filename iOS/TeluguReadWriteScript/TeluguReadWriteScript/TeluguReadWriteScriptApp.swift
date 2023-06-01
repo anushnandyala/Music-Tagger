c407762c16b9548c1229ad4f8029db63728b5fc3
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-06-01 18:35:19 -0400

    Fixed github issues

---
+++ iOS/TeluguReadWriteScript/TeluguReadWriteScript/TeluguReadWriteScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeluguReadWriteScriptApp.swift
+//  TeluguReadWriteScript
+//
+//  Created by Anush Nandyala on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TeluguReadWriteScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
