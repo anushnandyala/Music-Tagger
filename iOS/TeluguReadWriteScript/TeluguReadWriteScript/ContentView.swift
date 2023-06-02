9ad591a4b13f5c04b315e35bf84708d61d399136
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-06-01 22:57:44 -0400

    Added Color

--- iOS/TeluguReadWriteScript/TeluguReadWriteScript/ContentView.swift
+++ iOS/TeluguReadWriteScript/TeluguReadWriteScript/ContentView.swift
@@ -10,14 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            Image("teluguTitle")
-                .resizable()
-                .frame(width: 500, height: 250)
-                .aspectRatio(contentMode: .fit)
-                .padding(.all, 100.0)
+        ZStack {
+            Color(red: 0.243, green: 0.706, blue: 0.537) // #3eb489
+                .ignoresSafeArea()
+            
+            VStack {
                 
-            Text("Learn How to Read and Write in Telugu")
+                Image("teluguTitle")
+                    .resizable()
+                    .frame(width: 500, height: 250)
+                    .aspectRatio(contentMode: .fit)
+                    .padding([.top, .leading, .trailing], 100.0)
+                    
+                Text("Learn to Read and Write in Telugu").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.semibold).multilineTextAlignment(.center)
+            }
+            
         }
         
     }
