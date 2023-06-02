f8f55619089b80d3720e8576d32c08187fecdbd6
anushnandyala <112780752+anushnandyala@users.noreply.github.com>
2023-06-01 22:47:11 -0400

    More UI

--- iOS/TeluguReadWriteScript/TeluguReadWriteScript/ContentView.swift
+++ iOS/TeluguReadWriteScript/TeluguReadWriteScript/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Image("teluguTitle")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .padding(.all, 100.0)
+        VStack {
+            Image("teluguTitle")
+                .resizable()
+                .frame(width: 500, height: 250)
+                .aspectRatio(contentMode: .fit)
+                .padding(.all, 100.0)
+                
+            Text("Learn How to Read and Write in Telugu")
+        }
+        
     }
 }
 
